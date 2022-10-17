62b66933f40f7664d6a5dad918f8b649f285ba07
vacuum78 <115992369+vacuum78@users.noreply.github.com>
2022-10-17 14:50:09 +0300

    Add files via upload

---
+++ ЧЕРНИКОВ ДЕНИС/ЧЕРНИКОВ ДЕНИС/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЧЕРНИКОВ_ДЕНИС
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            System.Console.WriteLine("Привет, Яндекс!");
+            name = Console.ReadLine();
+            Console.WriteLine("Приятно познакомиться. " + name + "!");
+
+
+        }
+    }
+}
