43862c26ef26b4052162340822a13b482618d377
VACUUM <115992369+vacuum78@users.noreply.github.com>
2025-06-09 08:38:58 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,2 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ЧЕРНИКОВ_ДЕНИС_2_РАБОТА
-{ using static Console;
-    class HelloUser
-    {
-        static void Main(string[] args)
-        {
-            string name;
-            WriteLine("Введите ваше имя и фамилию: ");
-            name = ReadLine();
-            WriteLine("Индивидуальный гороскоп для пользователя" + name + "!");
-            WriteLine("Кем вы были в прошлой жизни:");
-            ReadLine();
-            WriteLine("Ваш знак зодиака");
-            name = ReadLine();
-            WriteLine("Ваш знак зодиака - " + name + ", поэтому вы тонко чувствующая натура");
-        }
-        привет !
-    }
-}
+ро
